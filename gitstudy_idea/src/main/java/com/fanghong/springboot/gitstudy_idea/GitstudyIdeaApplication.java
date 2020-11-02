05e2862607d61f9752bc54bc071e700eee031b78
方泓 <king1052369504@qq.com>
2020-11-03 02:41:03 +0800

    自建一个springboot空项目上传试试。

---
+++ gitstudy_idea/src/main/java/com/fanghong/springboot/gitstudy_idea/GitstudyIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.fanghong.springboot.gitstudy_idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstudyIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitstudyIdeaApplication.class, args);
+    }
+
+}
---
+++ gitstudy_idea/src/test/java/com/fanghong/springboot/gitstudy_idea/GitstudyIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanghong.springboot.gitstudy_idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyIdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
